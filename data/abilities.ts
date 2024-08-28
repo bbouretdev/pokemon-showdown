4f6d78784781eb4515a2f9862650b2286eb0b1b0
bbouretdev <bbouretdev@gmail.com>
2024-08-28 15:59:33 +0200

    test

--- data/abilities.ts
+++ data/abilities.ts
@@ -5916,8 +5916,8 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			// for (const ally of pokemon.allies()) {
 			// 	console.log(ally);
 			// }
-			console.log(pokemon.side.faintedThisTurn);
-			if (pokemon.side.faintedBeforeLastTurn) {
+			// console.log(pokemon.side.faintedThisTurn);
+			if (pokemon.side.faintedThisTurn) {
 				this.add('-ability', pokemon, 'Spiritual Vengeance');
 				this.boost({atk: 1}, pokemon);
 			}
