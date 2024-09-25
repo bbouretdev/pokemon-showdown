7ad44403516496ebd6dc661375d6120709dc1c50
bbouretdev <bbouretdev@gmail.com>
2024-09-25 17:13:18 +0200

    test

--- data/abilities.ts
+++ data/abilities.ts
@@ -7203,7 +7203,6 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			if (pokemon.side.faintedThisTurn) {
 				this.add('-ability', pokemon, 'Parental devotion');
 				this.boost({atk: 1, def: 1, spa: 1, spd: 1, spe: 1}, pokemon);
-				pokemon.tryTrap(true);
 			}
 		},
 		onTrapPokemon(pokemon) {
