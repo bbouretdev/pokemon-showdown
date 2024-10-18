2cf2d42e5374bd50c5a6249704a13a513ae6b020
bbouretdev <bbouretdev@gmail.com>
2024-10-18 19:22:51 +0200

    test

--- data/items.ts
+++ data/items.ts
@@ -7691,7 +7691,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 			if (!this.activeMove) throw new Error("Battle.activeMove is null");
 			if ((source && source !== pokemon) || this.activeMove.id === 'knockoff') {
 				//this.add('-enditem', pokemon, this.effect, '[weaken]');
-				this.damage(source.baseMaxhp / 2, source);
+				source.damage(source.baseMaxhp / 2);
 			}
 		},
 		num: 10001,
