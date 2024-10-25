39d8a0a44e3f48b7558f52fe7b379633ace41505
bbouretdev <bbouretdev@gmail.com>
2024-10-25 15:47:22 +0200

    test

--- data/items.ts
+++ data/items.ts
@@ -7688,6 +7688,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 			basePower: 160,
 		},
 		onTakeItem(item, pokemon, source) {
+			console.log(this.activeTarget);
 			if (!this.activeMove) throw new Error("Battle.activeMove is null");
 			if ((pokemon.foes()[0] && pokemon.foes()[0] !== pokemon) || this.activeMove.id === 'knockoff') {
 				console.log(pokemon.lastMove?.name);
