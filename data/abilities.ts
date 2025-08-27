6c950d700cb927d7d47ca27b0df6129fa3a0e215
bbouretdev <bbouretdev@gmail.com>
2025-08-27 17:22:36 +0200

    test

--- data/abilities.ts
+++ data/abilities.ts
@@ -8340,27 +8340,29 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 					}
 					if (move.isZ || move.isMax) return false;
 
-					// Iterate over move slots to find an empty one
-					let mimicIndex = pokemon.moveSlots.findIndex(slot => !slot);
+					// <!does not work!> -> always gives -1 Iterate over move slots to find an empty one
+					// let mimicIndex = pokemon.moveSlots.findIndex(slot => !slot);
+					// temporary fixed value to 1
+					let mimicIndex = 1;
 					console.log(mimicIndex);
-					// if (mimicIndex === -1) {
-					//   // If no empty slot, a random one is chosen
-					//   mimicIndex = this.random(4);
-					// }
+					if (mimicIndex === -1) {
+					  // If no empty slot, a random one is chosen
+					  mimicIndex = this.random(4);
+					}
 
 					// console.log(move);
 					// console.log(mimicIndex);
 
-					// pokemon.moveSlots[mimicIndex] = {
-					// 	move: move.name,
-					// 	id: move.id,
-					// 	pp: move.pp,
-					// 	maxpp: move.pp,
-					// 	target: move.target,
-					// 	disabled: false,
-					// 	used: false,
-					// 	virtual: true,
-					// };
+					pokemon.moveSlots[mimicIndex] = {
+						move: move.name,
+						id: move.id,
+						pp: move.pp,
+						maxpp: move.pp,
+						target: move.target,
+						disabled: false,
+						used: false,
+						virtual: true,
+					};
 					// this.add('-start', pokemon, 'Mimic', move.name);
 					// // Type part
 					// const targetTypes = target.getTypes();
